49d6b27fcb76f05e0eb683437e7d327a78373a0d
gouravrana7 <82229078+gouravrana7@users.noreply.github.com>
2021-04-11 13:14:44 +0530

    Add files via upload

---
+++ positiveNum.py
@@ -0,0 +1,17 @@
+list1 = [12, -7, 5, 64, -14]
+
+for i in list1:
+	
+	if i >= 0:
+
+	    print("list1 positive no",i)
+
+
+list2 = [12, 14, -95, 3]
+
+for j in list2:
+	
+	if j >= 0:
+
+	    print("list2 positive no",j)
+
